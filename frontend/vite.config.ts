76f8b6324d18b46e9bb9b237fc4d8e21b8e3f5ea
Warrain <Arain1@rambler.ru>
2025-12-11 23:11:04 +0000

    review 1.1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-      '/content': 'http://localhost:3000'
-    }
-  },
   css: {
     preprocessorOptions: {
       scss: {
